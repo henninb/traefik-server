dd783d55ad9b6b2c10f97f28bd1fab49a7c403fa
Brian Henning <henninb@msn.com>
2025-07-29 06:05:52 -0500

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM traefik:v2.10.1
+FROM traefik:v3.5.0
 # FROM traefik:v2.5.3
-COPY ./proxy.crt /etc/ssl/certs/proxy.crt
-COPY ./proxy.key /etc/ssl/private/proxy.key
+COPY ./brianhenning.fullchain.pem /etc/ssl/certs/
+COPY ./brianhenning.privkey.pem /etc/ssl/private/
+COPY ./bhenning.fullchain.pem /etc/ssl/certs/
+COPY ./bhenning.privkey.pem /etc/ssl/private/
 #COPY ./traefik.yml /etc/traefik/traefik.yml
-RUN mkdir -p /usr/local/share/ca-certificates
-COPY ./rootCA.pem /usr/local/share/ca-certificates/rootCA.pem
 RUN apk update
 RUN apk add -q --no-cache ca-certificates
 RUN update-ca-certificates
---
+++ run-docker.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+echo set -gx DOCKER_HOST tcp://192.168.10.10:2375
+echo export DOCKER_HOST=tcp://192.168.10.10:2375
+echo export DOCKER_HOST=ssh://henninb@192.168.10.10
+
+# doas cp /etc/letsencrypt/live/finance.bhenning.com/privkey.pem proxy.key
+# doas cp /etc/letsencrypt/live/finance.bhenning.com/fullchain.pem proxy.crt
+# doas chown henninb:henninb *.pem *.crt *.key
+
+docker context create remote --docker "host=ssh://henninb@192.168.10.10"
+# echo docker context use remote
+# docker context ls
+
+export DOCKER_HOST=ssh://henninb@192.168.10.10
+
+docker build -t traefik-server .
+
+docker images
+docker rm -f traefik-server
+
+docker run --name=traefik-server -h traefik-server --restart unless-stopped \
+  -p 80:80 -p 443:443 \
+  -v ./traefik.toml:/etc/traefik/traefik.toml \
+  -v ./dynamic:/etc/traefik/dynamic \
+  -d traefik-server
+
+docker ps -a
+
+exit 0
+
+# docker save traefik-server | docker --context remote load
+
+# echo export DOCKER_HOST=ssh://henninb@192.168.10.10
+# export DOCKER_HOST=tcp://192.168.10.10:2375
+# export DOCKER_HOST=ssh://henninb@192.168.10.10
+
+docker commit traefik-server traefik-server
+docker save traefik-server | docker --context remote load
+export DOCKER_HOST=ssh://henninb@192.168.10.10
+docker rm -f traefik-server
+docker run --name=traefik-server -h traefik-server --restart unless-stopped \
+  -p 80:80 -p 443:443 \
+  -v ./traefik.toml:/etc/traefik/traefik.toml \
+  -v ./dynamic:/etc/traefik/dynamic \
+  -d traefik-server
+docker ps -a
+
+exit 0
